11169671307c4f7da9d5efa23f5140172d6d83c3
Koech Nicholas <koechnicholas@gmail.com>
2015-06-27 11:02:17 +0300

    Added load progress bar and chart tab

--- js/app.js
+++ js/app.js
@@ -5,6 +5,17 @@
 var app = (function(){
     var households = {
 
+        globalVal: {
+            // Global variables
+            rcpPrecData45: "",
+            rcpPrecData85: "",
+            rcpTempData45: "",
+            rcpTempData85: "",
+            clickTemp: true,
+            clickPrec: true,
+            mapClickEvent: true
+        },
+
         initMap: function() {
 
             //Get layer  token
@@ -39,9 +50,14 @@ var app = (function(){
 
         resizeMapCanvasHeight: function(){
             $(window).on("resize", function() {
-                // Get browser window height and  assign it to map canvas
-                $(".map-canvas").height($(window).height());
+                // Set map window height
+                if ($(".tabbable").height() > $(window).height()){
+                    $(".map-canvas").height($(".tabbable").height() + 35);
+                }else{
+                    $(".map-canvas").height($(window).height());
+                }
                 map.invalidateSize();
+                $("#map-rcp-chart-container").width(($(window).width() - $(".map-canvas").width()) - 7); // Set chart container width
             }).trigger("resize");
         },
 
@@ -158,13 +174,32 @@ var app = (function(){
                 var mapData = e.data; // get grid layer data
                 var tabPaneHtml = "";
                 if (mapData) {
-                    $('#mapTab a[href="#map-rcp-tab"]').tab('show'); //show tab panel
-                    $(".map-rcp-title-bg,.map-rcp-body,.tab-pane-rcp p, br").remove(); //remove p and  br elements
+                    if ($(".nav-tabs .active").text() == "About") { // Show chart tab if about tab is active
+                        $('#mapTab a[href="#map-rcp-chart-tab"]').tab('show');
+                    }
+                    $(".map-rcp-title-bg,.map-rcp-body").remove(); //remove p and  br elements
                     households.showMapEmissionsScenarioTitle(mapData); //show emission titles
                     var tabPaneHtmlScenario85 =  households.showMapEmissionsScenario(mapData, "8_", "8.5"); //show emission scenario 8.5
                     var tabPaneHtmlScenario45 =  households.showMapEmissionsScenario(mapData, "4_", "4.5"); //show emission scenario 4.5
-                    //Add source to tab panel html"
-                    tabPaneHtml = '<div class="map-rcp-body">' + tabPaneHtmlScenario85 + tabPaneHtmlScenario45 + '<div class="map-rcp-source"><span>Source: <a href="http://climatewizard.ciat.cgiar.org/" target="_blank">&copy; Climate Wizard 2015 </a></span></div></div>';
+                    // Chart html
+                    tabPaneHtml = '<div class="map-rcp-body"><div class="row"><div class="tab-pane-rcp-btn"><div class="col-xs-6"><a class="btn btn-large btn-warning btn-md" id="btnTemp" onclick="app.showMapScenarioTempChartOnBtnClick();"><span class="btn-label"> Temperature </span><span class="glyphicon glyphicon-chevron-down" aria-hidden="true"></span></a></div><div class="col-xs-6"><a class="btn btn-large btn-success btn-md pull-right" id="btnPrec" onclick="app.showMapScenarioPrecChartOnBtnClick();"><span class="btn-label"> Precipitation </span><span class="glyphicon glyphicon-chevron-down" aria-hidden="true"></span></a></div></div></div><div class="map-rcp-chart"><div id="map-rcp-chart-container"></div><div class="map-rcp-source"><span>Source: <a href="http://climatewizard.ciat.cgiar.org/" target="_blank">&copy; Climate Wizard 2015 </a></span></div></div></div>';
+                    $('.tab-pane-rcp-chart').append(tabPaneHtml); // Append html to existing html class to show chart
+                    $("#map-rcp-chart-container").width(($(window).width() - $(".map-canvas").width()) - 7); // Set chart container width
+                    //show temperature and precipitation scenario chart for rcp 8.5 and 4.5
+                    if  (households.globalVal.mapClickEvent) {
+                        households.showMapEmissionsScenarioChart("FUTURE TEMPERATURE CHANGE", tabPaneHtmlScenario85.rcpTempData, tabPaneHtmlScenario45.rcpTempData);
+                        households.globalVal.clickTemp = false;
+                    } else{
+                        households.showMapEmissionsScenarioChart("CHANGE IN PRECIPITATION", tabPaneHtmlScenario85.rcpPrecData, tabPaneHtmlScenario45.rcpPrecData);
+                    }
+                    // Assign rcp 8.5 and 4.5 temperature data to global variables
+                    households.globalVal.rcpTempData45 = tabPaneHtmlScenario85.rcpTempData;
+                    households.globalVal.rcpTempData85 = tabPaneHtmlScenario45.rcpTempData;
+                    // Assign rcp 8.5 and 4.5 precipitation data to global variables
+                    households.globalVal.rcpPrecData45 = tabPaneHtmlScenario85.rcpPrecData;
+                    households.globalVal.rcpPrecData85 = tabPaneHtmlScenario45.rcpPrecData;
+                    //show temperature scenario text for rcp 8.5 and 4.5
+                    tabPaneHtml = '<div class="map-rcp-body">' + tabPaneHtmlScenario85.tabPaneHtml + tabPaneHtmlScenario45.tabPaneHtml + '<div class="map-rcp-source"><span>Source: <a href="http://climatewizard.ciat.cgiar.org/" target="_blank">&copy; Climate Wizard 2015 </a></span></div></div>';
                     $('.tab-pane-rcp').append(tabPaneHtml);
                 }
             });
@@ -187,16 +222,19 @@ var app = (function(){
                 tabPaneHtml += '<p class="map-rcp-region-title"> Region: No Record </p></div>';
             }
             $('.tab-pane-rcp').append(tabPaneHtml);
+            $('.tab-pane-rcp-chart').append(tabPaneHtml);
         },
 
         showMapEmissionsScenario: function(mapData, searchRcp, rcpCategory){
             var count_rcp = 0;
             var count_pc = 0;
             var tabPaneHtml = "";
+            var rcpTempData = [];
+            var rcpPrecData = [];
             for (var key in mapData) {
                 if (mapData.hasOwnProperty(key)) { //check if data has key
                     if (key !== "No_HSEHOLD" && key !== "RURAL_POP") {
-                        //Display emission scenario 8.5
+                        //Display emission scenario
                         if (key.indexOf(searchRcp)!== -1){
                             //Section emmision and temperature titles
                             if (count_rcp < 1){
@@ -207,11 +245,14 @@ var app = (function(){
                             if (key.indexOf("AV_TMP")!== -1){
                                 if (key.indexOf("_30")!== -1){
                                     tabPaneHtml += '<p class="map-rcp"><span class="map-rcp-year-label">2030: </span><span class="map-rcp-year-data">' + mapData[key] + '&deg;C</span></p>';
+                                    rcpTempData.push(mapData[key]);
                                 }
                                 else if (key.indexOf("_50")!== -1){
                                     tabPaneHtml += '<p class="map-rcp"><span class="map-rcp-year-label">2050: </span><span class="map-rcp-year-data">' + mapData[key] + '&deg;C</span></p>';
+                                    rcpTempData.push(mapData[key]);
                                 }else{
                                     tabPaneHtml += '<p class="map-rcp"><span class="map-rcp-year-label">2080: </span><span class="map-rcp-year-data">' + mapData[key] + '&deg;C</span></p>';
+                                    rcpTempData.push(mapData[key]);
                                 }
                             }else{
                                 //Precipitation section title
@@ -221,11 +262,14 @@ var app = (function(){
                                 //List change in precipitation
                                 if (key.indexOf("_30")!== -1){
                                     tabPaneHtml += '<p class="map-rcp"><span class="map-rcp-year-label">2030: </span></span><span class="map-rcp-year-data">' + mapData[key] + '</span></p>';
+                                    rcpPrecData.push(+mapData[key].substring(0, mapData[key].length - 1)); // Remove trailing % symbol and convert to Number type
                                 }
                                 else if (key.indexOf("_50")!== -1){
                                     tabPaneHtml += '<p class="map-rcp"><span class="map-rcp-year-label">2050: </span></span><span class="map-rcp-year-data">' + mapData[key] + '</span></p>';
+                                    rcpPrecData.push(+mapData[key].substring(0, mapData[key].length - 1));
                                 }else{
                                     tabPaneHtml += '<p class="map-rcp"><span class="map-rcp-year-label">2080: </span></span><span class="map-rcp-year-data">' + mapData[key] + '</span></p>';
+                                    rcpPrecData.push(+mapData[key].substring(0, mapData[key].length - 1));
                                 }
                                 count_pc += 1;
                             }
@@ -234,7 +278,58 @@ var app = (function(){
                     }
                 }
             }
-            return tabPaneHtml;
+            return {
+                tabPaneHtml: tabPaneHtml,
+                rcpTempData: rcpTempData,
+                rcpPrecData: rcpPrecData
+            };
+        },
+
+        showMapEmissionsScenarioChart: function(chartTitle, scenario85Data, scenario45Data){
+            var scenarioChart = new Highcharts.Chart({
+                chart: {
+                    type: 'column',
+                    renderTo: 'map-rcp-chart-container'
+                },
+                colors: ["#42696E","#F24C27"],
+                title: {
+                    text: chartTitle,
+                    style: {"fontSize": "14px"}
+                },
+                xAxis: {
+                    categories: ['2030', '2050', '2080']
+                },
+                credits: {
+                    enabled: false
+                },
+                series: [{
+                    name: 'RCP-8.5',
+                    data: scenario85Data
+                }, {
+                    name: 'RCP-4.5',
+                    data: scenario45Data
+                }]
+            });
+        },
+
+        showMapScenarioTempChartOnBtnClick: function(){
+            // Load chart values on temperature button click
+            if  (households.globalVal.clickTemp){
+                households.showMapEmissionsScenarioChart("FUTURE TEMPERATURE CHANGE", households.globalVal.rcpTempData85, households.globalVal.rcpTempData45);
+            }
+            households.globalVal.clickTemp = false; // Disable temperature button
+            households.globalVal.clickPrec = true; // Enable precipitation button
+            households.globalVal.mapClickEvent = true; // Show temperature scenario chart
+        },
+
+        showMapScenarioPrecChartOnBtnClick: function(){
+            // Load chart values on precipitation button click
+            if  (households.globalVal.clickPrec ){
+                households.showMapEmissionsScenarioChart("CHANGE IN PRECIPITATION", households.globalVal.rcpPrecData85, households.globalVal.rcpPrecData45);
+            }
+            households.globalVal.clickTemp = true;
+            households.globalVal.clickPrec = false;
+            households.globalVal.mapClickEvent = false; // Show precipitation scenario chart
         }
     };
 
